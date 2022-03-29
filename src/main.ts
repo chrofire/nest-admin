8feda1427a915e2ecf0651257939b321ffd8087a
chrofire <451939778@qq.com>
2022-03-29 20:56:36 +0800

    feat: 添加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import useConfig from 'config'
+
+const config = useConfig()
 
 async function bootstrap () {
     const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+
+    // swagger
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('nest-admin')
+        .setDescription('nest-admin接口')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build()
+    const swaggerOptions: SwaggerDocumentOptions = {}
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig, swaggerOptions)
+    SwaggerModule.setup('swagger', app, swaggerDocument)
+
+    await app.listen(config.port)
+
+    Logger.log(`App running at: http://localhost:${config.port}`, 'NestApplication')
+    Logger.log(`Swagger: http://localhost:${config.port}/swagger`, 'Swagger')
 }
 bootstrap()
